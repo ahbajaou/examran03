1df6b56d8e7f2b6c921c099fa462bf0f796deb6e
Ahmed Bajaou <ahbajaou@c4r4p2.1337.ma>
2023-10-08 23:24:30 +0100

    push

--- ft_printf.c
+++ ft_printf.c
@@ -1,50 +1,50 @@
-#include <stdio.h>
-#include <stdarg.h>
+
 #include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
 
-
-void    ft_putstr(const char *str, int *len)
+void    ft_putstr(char *str,int *len)
 {
     int i = 0;
-    if (!str)
-        write(1,"(null)\n",8);
+    if(!str)
+        str = "(null)";
     while (str[i])
-    {
-        *len += write(1,&str[i],1);
-        i++;
-    }
+        *len += write(1,&str[i++],1);
 }
 
-void    ft_putnbr(int nbr,int *len,int base)
+void    ft_putnbr_hex(long long int nbr,int base,int *len)
 {
-    char *hex = "0123456789abcdef";
-    if (nbr < 0)
+    char    *hex = "0123456789abcdef";
+
+    if(nbr < 0)
     {
         nbr *= -1;
-        len += write(1,"-",1);
+        *len += write(1,"-",1);
     }
-    if (nbr >= base)
-        ft_putnbr((nbr / base),len,base);
-    len += write(1,&hex[nbr % base],1);
+    if(nbr >= base)
+        ft_putnbr_hex((nbr / base),base,len);
+    *len += write(1,&hex[nbr % base],1);
 }
+
 int ft_printf(const char *str, ...)
 {
     int i = 0;
-    int len = 0 ;
-    va_list ptr;
+    int len = 0;
 
+    va_list ptr;
     va_start(ptr,str);
-    while (str[i])
+
+    while(str[i])
     {
         if (str[i] == '%')
         {
             i++;
-            if (str[i] == 's')
-                ft_putstr(va_arg(ptr, char *),&len);
-            if (str[i] == 'd')
-                ft_putnbr(va_arg(ptr,int),&len,10);
-            if (str[i] == 'x')
-                ft_putnbr(va_arg(ptr,int),&len,16);
+            if(str[i] == 's')
+                ft_putstr(va_arg(ptr,char *),&len);
+            else if(str[i] == 'd')
+                ft_putnbr_hex((long long int)va_arg(ptr,int),10,&len);
+            else if(str[i] == 'x')
+                ft_putnbr_hex((long long int)va_arg(ptr,unsigned int),16,&len);
         }
         else
             len += write(1,&str[i],1);
